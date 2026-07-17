986cd221db659d046de8eaff77c08aed7eb0ba45
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-17 19:33:59 +0530

    one optimized

---
+++ one_optimized.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 9;
+
+    // n should be odd
+    int mid = n / 2 + 1;
+
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j <= n; j++) {
+
+            if(i == mid || j == mid)
+                printf("* ");
+            else
+                printf("  ");
+
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
