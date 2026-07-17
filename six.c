ade60bcc5db7f91bd916bdafb2a0e860d7a5697e
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-17 22:22:13 +0530

    six

--- six.c
+++ six.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-int main ( ){
+int main ( ) {
     int n = 5; 
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1; j<=i-1;j++){
+    for(int i = 1 ; i <= 2*n-1 ; i++){
+        int star = n-i+1;
+        int space = i-1;
+        if(i > n){
+            star = i-n+1;
+            space = 2*n - i -1;
+        }
+        for(int j = 1; j <=space;j++){
             printf(" ");
         }
-        for(int j = 1 ; j <=n; j++){
-            if(i==1 || j == 1 || j == n || i==n){
-                printf("*");
-            }
-            else{
-                printf(" ");
-            }
-            
+        
+        for(int j = 1 ; j <=star;j++){
+            printf("*");
         }
-
         printf("\n");
     }
     return 0;
